15396c99623369afebd4c836b89adf7837a85dd5
JX003 <32142980+JX003@users.noreply.github.com>
2018-10-29 19:07:48 -0400

    Added Ban & Kick commands
    Checks if user has perms to kick/ban before doing it

--- litebot.py
+++ litebot.py
@@ -8,17 +8,17 @@ bot = Bot(command_prefix="!")
 
 @bot.event
 async def on_read():
-    print("Client logged in")
+	print("Client logged in")
 
 #Welcome @user
 @bot.async_event
 async def on_member_join(Member : discord.User):
-    await bot.send_message(bot.get_channel(Member.server.id),"Welcome **"+Member.mention+"**")
+	await bot.send_message(bot.get_channel(Member.server.id),"Welcome **"+Member.mention+"**")
 
 #user has left the server
 @bot.async_event
 async def on_member_remove(Member : discord.User):
-    await bot.send_message(bot.get_channel(Member.server.id),"**"+Member.name+"** has left the server")
+	await bot.send_message(bot.get_channel(Member.server.id),"**"+Member.name+"** has left the server")
 
 #Deletes messages that include words
 @bot.event
@@ -29,12 +29,39 @@ async def on_message(message, timeout=10):
 			await bot.delete_message(message)
 			await bot.send_message(message.channel, 'No swearing')
 			print ("Deleted Message By: " + str(message.author))
+			
+	await bot.process_commands(message)
 
+@bot.command (pass_context=True)
+async def kick(ctx, Member : discord.User):
+	if (ctx.message.author.server_permissions.kick_members == True or ctx.message.author.server_permissions.administrator == True):
+		try: 
+			await bot.kick(Member)
+			await bot.say("Successfully kicked **" + Member.name + "**")
+		except discord.HTTPException:
+			await bot.say("Unable to kick **" + Member.name + "**.")
+	else:
+		await bot.say("You do not have permission to kick** " + Member.name + "**")			
+@bot.command (pass_context=True)
+async def ban(ctx, Member : discord.User):
+	if (ctx.message.author.server_permissions.ban_members == True or ctx.message.author.server_permissions.administrator == True):
+		try: 
+			await bot.ban(Member, delete_message_days=3)
+			await bot.say("Successfully banned **" + Member.name + "**")
+		except discord.HTTPException:
+			await bot.say("Unable to ban **" + Member.name + "**")			
+	else:
+		await bot.say("You do not have permission to ban **" + Member.name + "**")
+	
+#settings.owner
+	
 print ('Lite-Bot is activating')
 print ('Ready')
 print ('')
 print ('(ᵔᴥᵔ)')
 print ('')
+
+
 
 #Opens words file
 f = open('words.txt', 'r')
